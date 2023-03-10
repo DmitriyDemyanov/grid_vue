34452586c3b6473fa909edde7ece1fe55058b615
Dmitry Demyanov <dimaprostokosmos@gmail.comnpm run servegit config --global user.email dimaprostokosmos@gmail.com>
2023-03-10 20:02:27 +0200

    create modal sign

--- src/components/ModalSignIn.vue
+++ src/components/ModalSignIn.vue
@@ -1,5 +1,12 @@
 <template>
-  <b-modal id="modal-1">
+  <b-modal
+    modal-class="modal-sign"
+    id="modal-1"
+    hide-header-close
+    modal-header
+    centered
+  >
+    <div class="modal-title">Please enter your details!</div>
     <div>
       <label class="label-name mt-3" for="input-name">Your Name:</label>
       <input
@@ -10,7 +17,7 @@
         required
       />
 
-      <div>
+      <div class="wrap-email">
         <label class="label-email" for="input-email">Your Email:</label>
         <input
           id="input-email"
@@ -20,6 +27,10 @@
           required
         />
       </div>
+    </div>
+    <div class="wrapper-btn d-flex justify-content-between">
+      <button class="cancel-btn btn">cancel</button>
+      <button class="sign-in-btn btn">sign in</button>
     </div>
   </b-modal>
 </template>
@@ -39,6 +50,47 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+::v-deep .modal-sign {
+  .modal-header,
+  .modal-footer {
+    display: none;
+  }
+  .modal-title {
+    color: var(--main-color-purple);
+    font-size: 30px;
+    text-align: center;
+  }
+}
+.wrapper-btn {
+  width: 100%;
+  height: 50px;
+  margin: 20px 0;
+}
+
+.btn {
+  width: calc(50% - 15px);
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.cancel-btn {
+  border-radius: 20px;
+  border: solid 3px var(--main-color-orange);
+  &:hover {
+    background-color: var(--main-color-orange);
+  }
+}
+.sign-in-btn {
+  border-radius: 20px;
+  border: solid 3px var(--main-color-purple);
+  &:hover {
+    background-color: var(--main-color-purple);
+  }
+}
+.wrap-email {
+  margin-top: 20px;
+}
 input {
   width: 100%;
   height: 48px;
